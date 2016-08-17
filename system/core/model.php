99b02e076d414b26d87d10b4bd34949ed2351da4
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-08-17 10:41:28 +0200

    load_password_compatibility() metodus hozzaadva

--- system/core/model.php
+++ system/core/model.php
@@ -34,6 +34,20 @@ class Model {
 		$this->connect = db::close_connect();
 	}
 
+
+    /**
+     * Jelszó kompatibilitás library betöltése
+     *
+     * @access private
+     */
+    protected function load_password_compatibility()
+    {
+        if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+            // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+            // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+            require_once(LIBS . '/password_compatibility_library.php');
+        }
+    } 
 	
 	/**
 	 * Adat lekérdezése egy táblából 
