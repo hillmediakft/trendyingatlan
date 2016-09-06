69bc0aa2c6e1f5c892ad833cd4307f75de71c08c
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-09-06 13:22:19 +0200

    logintodb class modositas

--- system/libs/logintodb.php
+++ system/libs/logintodb.php
@@ -2,7 +2,7 @@
 /**
 * Az adatbázis logs táblájába létrehoz egy új rekordot
 */
-class Log_into
+class LogIntoDb
 {
 	private $connect;	
 	private $query;	
@@ -16,20 +16,20 @@ class Log_into
 	/**
 	 * Új rekord létrehozása a logs táblában
 	 */
-    public function index($message)
+    public function index($type, $message)
     {
 /*
         $sth = $this->connect->prepare('INSERT INTO `logs` (user_id, message, date) VALUES(:user_id, :message, :date)');
         $sth->execute(array(
         		':user_id' => Session::get('user_id'),
-	        	':message' => $message,
+	        	':message' => '<span class="badge badge-sm">' . $type . '</span> ' . $message,
 	        	':date' => date("Y-m-d h:i:s")
         	));
 */
 
         $data = array (
 	        'user_id' => Session::get('user_id'),
-	        'message' => $message,
+	        'message' => '<span class="badge badge-sm">' . $type . '</span> ' . $message,
 	        'date' => date("Y-m-d h:i:s")
         );
         $this->query->set_table(array('logs'));
