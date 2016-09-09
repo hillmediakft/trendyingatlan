9d4876e1cd9a9479b3c69a884225cd81510126a0
varnagy-attila <vucuka@gmail.com>
2016-09-09 14:32:37 +0200

    módosítás a string_to_slug metódusban

--- system/libs/util_class.php
+++ system/libs/util_class.php
@@ -223,8 +223,8 @@ class Util {
      * 	minden karaktert kisbetűre cserél
      */
     public static function string_to_slug($string) {
-        $accent = array("&", " ", "-", "á", "é", "í", "ó", "ö", "ő", "ú", "ü", "ű", "Á", "É", "Í", "Ó", "Ö", "Ő", "Ú", "Ü", "Ű");
-        $no_accent = array('and', '_', '_', 'a', 'e', 'i', 'o', 'o', 'o', 'u', 'u', 'u', 'A', 'E', 'I', 'O', 'O', 'O', 'U', 'U', 'U');
+        $accent = array("?", "!", ":", "&", " ", "-", "á", "é", "í", "ó", "ö", "ő", "ú", "ü", "ű", "Á", "É", "Í", "Ó", "Ö", "Ő", "Ú", "Ü", "Ű");
+        $no_accent = array('', '', '-', 'and', '-', '-', 'a', 'e', 'i', 'o', 'o', 'o', 'u', 'u', 'u', 'A', 'E', 'I', 'O', 'O', 'O', 'U', 'U', 'U');
         $string = str_replace($accent, $no_accent, $string);
         $string = strtolower($string);
         return $string;
@@ -275,6 +275,45 @@ class Util {
         } else {
             return false;
         }
+    }  
+    
+    /**
+     * Egy szövegből az elejétől kezdődően adott karakterszámú rész ad vissza szóra kerekítve
+     *  
+     * @param   string  $string  szöveg
+     * @param   int  $char  karakterek száma
+     * @return  string  a levágott szöveg
+     */
+    public static function substr_word($string, $char) {
+        $s = mb_substr($string, 0, $char, 'UTF-8');
+        $result = substr($s, 0, strrpos($s, ' '));
+        return $result;
+    }
+
+    /**
+     * Egy szövegből az elejétől adott számú mondatot ad vissza
+     *  
+     * @param   string  $body  szöveg
+     * @param   int  $sentencesToDisplay  a mondatk száma
+     * @return  string  a levágott szöveg
+     */
+    static function sentence_trim($body, $sentencesToDisplay = 1) {
+        $nakedBody = preg_replace('/\s+/', ' ', strip_tags($body));
+        $sentences = preg_split('/(\.|\?|\!)(\s)/', $nakedBody);
+
+        if (count($sentences) <= $sentencesToDisplay)
+            return $nakedBody;
+
+        $stopAt = 0;
+        foreach ($sentences as $i => $sentence) {
+            $stopAt += strlen($sentence);
+
+            if ($i >= $sentencesToDisplay - 1)
+                break;
+        }
+
+        $stopAt += ($sentencesToDisplay * 2);
+        return trim(substr($nakedBody, 0, $stopAt));
     }    
 
 }
